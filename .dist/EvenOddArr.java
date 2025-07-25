c4c6bb160beab98a5f690af24ca5150113bd4d15
Nooremuskan <nooremuskan51@gmail.com>
2025-07-25 21:51:46 +0530

    initial commit

---
+++ .dist/EvenOddArr.java
@@ -0,0 +1,19 @@
+public class EvenOddArr{
+    public static void main(String args[]){
+
+        int[] evenNumbers = new int[20];
+        int[] oddNumbers = new int[20];
+        int evenSum = 0;
+        int oddSum = 0;
+
+        for(int i=0; i<20; i++){
+            evenNumbers[i]=(i+1)*2;
+            oddNumbers[i]=(2*i)+1;
+
+            evenSum += evenNumbers[i];
+            oddSum += oddNumbers[i];
+        }
+        System.out.println("Sum of EvenNumbers :" +evenSum);
+        System.out.println("Sum of oddNumbers :" +oddSum);
+    }
+}
